0b5f9f9cc42d227621a089448a35639d27b15de0
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-06-27 19:59:34 +0530

    Add files via upload

---
+++ 11_user_input.py
@@ -0,0 +1,11 @@
+a = input("Enter your name:")
+print("My name is",a)
+x =input("Enter first number: ")
+y = input("Enter second number:")
+print("Sum is",x+y)
+
+var1 = int(input())
+var2 = float(input())
+
+print()
+print(int(x) + int(y))
