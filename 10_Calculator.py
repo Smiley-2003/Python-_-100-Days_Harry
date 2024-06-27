0180b1ef396d5a18498b660dd705e57456524e9d
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-06-27 18:11:46 +0530

    Add files via upload

---
+++ 10_Calculator.py
@@ -0,0 +1,21 @@
+#Day - 8 Create a calculator using Python
+#Name: Naina Mogha
+#Date 27 June 2024
+
+# var a = 19
+# var b = 10
+
+a = 3
+b = 19
+
+sum = a + b
+print("The sum of",a,"+", b,"=",sum)
+
+diff = a - b
+print("The difference of ",a ,"-", b ,"=",diff)
+
+mul = a * b
+print("The multiplication of",a ,"*",b,"=",mul)
+
+div = a / b
+print("The value of ",a,"/",b,"=" ,div)
